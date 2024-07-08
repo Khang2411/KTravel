cd2d352f77f2cdcc86a50d287789fa677d1d5c05
Khang2411 <hk24112000@gmail.com>
2024-07-08 19:03:24 +0700

    update category

--- components/category/mutateCategory.ts
+++ components/category/mutateCategory.ts
@@ -2,6 +2,7 @@
 import { revalidatePath } from 'next/cache'
 import { redirect } from 'next/navigation';
 
-export default async function mutateCategory(search: string) {
-    return redirect(search)
+export default async function mutateCategory() {
+    return revalidatePath('/', 'page')
+    // return redirect(search)
 }
--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/api/auth/:path*",
+        destination: "/api/auth/:path*",
+      },
+      {
+        source: "/api/:path*",
+        destination: `${process.env.NEXT_PUBLIC_API_URL}/api/:path*`, // Proxy to Backend
+      },
+    ];
+  },
   images: {
     dangerouslyAllowSVG: true,
     remotePatterns: [
