8320fc83a2a1e81ae4acc8c372248d626c9ef6d0
Khang2411 <hk24112000@gmail.com>
2024-07-08 18:30:47 +0700

    rm proxy

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: "/api/auth/:path*",
-        destination: "/api/auth/:path*",
-      },
-      {
-        source: "/api/:path*",
-        destination: `${process.env.NEXT_PUBLIC_API_URL}/api/:path*`, // Proxy to Backend
-      },
-    ];
-  },
   images: {
     dangerouslyAllowSVG: true,
     remotePatterns: [
