db421538b1d6abfdb2c24ddddc8e455d75419d9a
Khang2411 <hk24112000@gmail.com>
2024-07-09 03:14:13 +0700

    undici

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,34 +1,43 @@
-import { NextAuthProvider } from '@/components/common';
-import '@fontsource/roboto/500.css';
-import { Box } from '@mui/material';
-import { Inter } from 'next/font/google';
-import './globals.css';
-import type { Metadata } from 'next'
+import { NextAuthProvider } from "@/components/common";
+import "@fontsource/roboto/500.css";
+import { Box } from "@mui/material";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import type { Metadata } from "next";
+import { Agent, setGlobalDispatcher } from "undici";
 
-const inter = Inter({ subsets: ['latin'] })
+setGlobalDispatcher(new Agent({ connect: { timeout: 60_000 } }));
+
+const inter = Inter({ subsets: ["latin"] });
 
 export const maxDuration = 60;
 
 export const metadata: Metadata = {
-  title: 'Du lịch | Nhà nghỉ dưỡng Ktravel',
-  description: 'Đặt chỗ cho mọi phòng, nơi ở du lịch khắp châu á',
-  keywords: ['du lịch', 'cho thuê', 'phòng ở', 'du lich', 'cho thue','phong o' ,'Ktravel'],
+  title: "Du lịch | Nhà nghỉ dưỡng Ktravel",
+  description: "Đặt chỗ cho mọi phòng, nơi ở du lịch khắp châu á",
+  keywords: [
+    "du lịch",
+    "cho thuê",
+    "phòng ở",
+    "du lich",
+    "cho thue",
+    "phong o",
+    "Ktravel",
+  ],
   metadataBase: new URL("https://www.ktravel.online"),
-}
+};
 export default function RootLayout({
-  children
+  children,
 }: {
-  children: React.ReactNode,
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-        <NextAuthProvider>
-          <body className={inter.className}>
-            <Box component={'main'}>
-              {children}
-            </Box>
-          </body>
-        </NextAuthProvider>
+      <NextAuthProvider>
+        <body className={inter.className}>
+          <Box component={"main"}>{children}</Box>
+        </body>
+      </NextAuthProvider>
     </html>
-  )
+  );
 }
